69902fa794351adc2c25ec3a243fd72800468e9a
Niklas Thorne <notrupertthorne@gmail.com>
2014-02-05 21:03:26 +0100

    Added license information

--- seed_repo.py
+++ seed_repo.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2014 Niklas Thorne.
+
+# This file is part of seed_repo.
+#
+# seed_repo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# seed_repo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with seed_repo.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """ Initialize a repo with some commits. """
 
 import argparse
